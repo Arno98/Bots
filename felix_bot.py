225f023c8c945a3e1497f3833ed914a4f832c4ad
Roma Pavlov <49715263+Arno98@users.noreply.github.com>
2020-04-17 22:21:46 +0300

    Add files via upload
    @felix_007bot

---
+++ felix_bot.py
@@ -0,0 +1,20 @@
+import telebot
+
+bot = telebot.TeleBot('1120238645:AAGB2sNYwphJXXEVfTAeKSCq4PXUqJOTk_0')
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard.row('Hello', 'How are you?', 'Goodbye')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id, "Hello, I am your first bot!", reply_markup=keyboard)
+	
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+	if message.text.lower() == 'hello':
+		bot.send_message(message.chat.id, "Hello, creator!")
+	elif message.text.lower() == 'goodbye':
+		bot.send_message(message.chat.id, "Goodbye, creator!")
+	elif message.text.lower() == 'how are you?':
+		bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAJJP16Z_RTRD1XRYEZoDIRJmsnQ4CQEAAKgCQACeVziCZ1X90THK6XzGAQ')
+		
+bot.polling()
