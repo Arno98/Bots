61452e2079e6f97b82892584df51af1e38427c13
Roma Pavlov <49715263+Arno98@users.noreply.github.com>
2021-05-13 01:20:54 +0300

    Update call_api.py

--- Exchange_Rate_Bot/call_api.py
+++ Exchange_Rate_Bot/call_api.py
@@ -2,7 +2,7 @@ import requests
 
 def call_api(url, params=None, headerss=False):
 	headers = {
-		'x-rapidapi-key': "fba481570emsh5cac1f6f37eb44cp1cfc82jsnc77f26030c2d",
+		'x-rapidapi-key': "api_key",
 		'x-rapidapi-host': "currency-converter5.p.rapidapi.com"
 		}
 	if headerss == True:
